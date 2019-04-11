1e18afc40b77a7d34cda2f5cfb3b194d7ad9f4d7
Ben Anderson <banderson@quantros.com>
2019-04-10 21:02:30 -0400

    Added initial unit tests

---
+++ StatisticsCalculatorTests/StatisticsCalculatorTests.cs
@@ -0,0 +1,27 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StatisticsCalculator;
+
+namespace StatisticsCalculatorTests
+{
+    [TestClass]
+    public class StatisticsCalculatorTests
+    {
+        [DataTestMethod]
+        [DataRow(new[] { 5, 15 }, 10)]
+        public void Mean(int[] inputs, double expected)
+        {
+            var actual = StatsCalculator.Mean(inputs);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [DataTestMethod]
+        [DataRow(new[] { 5, 15 }, 10)]
+        [DataRow(new[] { 5, 8, 15 }, 8)]
+        public void Median(int[] inputs, double expected)
+        {
+            var actual = StatsCalculator.Median(inputs);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
